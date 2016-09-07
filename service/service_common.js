17ec522baca807b7445dfcbef40a6998892d6c86
root <jeff@ossph.com>
2016-09-07 01:10:34 -0400

    new file:   service/service_management/doctype/service_sales_tax_and_charges_template/__init__.py 	new file:   service/service_management/doctype/service_sales_tax_and_charges_template/service_sales_tax_and_charges_template.js 	new file:   service/service_management/doctype/service_sales_tax_and_charges_template/service_sales_tax_and_charges_template.json 	new file:   service/service_management/doctype/service_sales_tax_and_charges_template/service_sales_tax_and_charges_template.py 	new file:   service/service_management/doctype/service_sales_tax_and_charges_template/test_service_sales_tax_and_charges_template.py

---
+++ service/service_common.js
@@ -0,0 +1,2 @@
+cur_frm.cscript.tax_table = "Sales Taxes and Charges";
+{% include 'erpnext/accounts/doctype/sales_taxes_and_charges_template/sales_taxes_and_charges_template.js' %}
---
+++ service/service_management/doctype/service_sales_tax_and_charges_template/service_sales_tax_and_charges_template.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2016, www.ossph.com and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Service Sales Tax and Charges Template', {
+	refresh: function(frm) {
+
+	}
+});
